5a1b426f6a0111840ac899ac03e6361d83dc028e
rock-tran <rock.tran@manadr.com>
2022-10-12 15:12:42 +0700

    Add Python support

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -41,3 +41,8 @@ autocmd("VimLeavePre", {
 })
 
 require "custom.commands"
+
+
+vim.g.loaded_python3_provider = nil
+vim.g.python3_host_prog = '/usr/bin/python3'
+vim.g.python_host_prog = '~/.pyenv/versions/2.7.18/bin/python'
