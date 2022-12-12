7e386177c45e30e96274629a51088ad7c95ddf4c
rock-tran <rock.tran@manadr.com>
2022-12-12 16:14:28 +0700

    Update ruby config

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -11,6 +11,10 @@ vim.filetype.add {
   }
 }
 
+
+vim.g.loaded_ruby_provider = nil
+vim.g.ruby_host_prog = "/usr/local/rvm/gems/ruby-3.1.2/bin/neovim-ruby-host"
+
 vim.g.loaded_python3_provider = nil
 vim.g.python3_host_prog = '/usr/bin/python3'
 vim.g.python_host_prog = '~/.pyenv/versions/2.7.18/bin/python'
--- nvchad/custom/plugins/lspconfig.lua
+++ nvchad/custom/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "jsonls", "tsserver", 'tailwindcss' , 'bashls', "pyright", "ruby_ls", "lemminx" }
+local servers = { "html", "cssls", "clangd", "jsonls", "tsserver", 'tailwindcss' , 'bashls', "pyright", "lemminx", "solargraph" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
