047a86ade81c01644ea20bf683f5f5db630ab0c9
rock-tran <rock.tran@manadr.com>
2022-12-22 18:00:40 +0700

    Configure jdtls

--- nvchad/custom/plugins/lspconfig.lua
+++ nvchad/custom/plugins/lspconfig.lua
@@ -1,12 +1,54 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "jsonls", "tsserver", 'tailwindcss' , 'bashls', "pyright", "lemminx", "solargraph" }
+local lspconfig = require("lspconfig")
+local servers = {
+	"html",
+	"cssls",
+	"clangd",
+	"jsonls",
+	"tsserver",
+	"tailwindcss",
+	"bashls",
+	"pyright",
+	"lemminx",
+	"solargraph",
+	"gradle_ls",
+}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
+end
+
+local java_17_home = os.getenv("JAVA_17_HOME")
+if java_17_home then
+	local jdtls_path = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/jdtls"
+
+	lspconfig.jdtls.setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		cmd = {
+			java_17_home .. "/bin/java",
+			"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+			"-Dosgi.bundles.defaultStartLevel=4",
+			"-Declipse.product=org.eclipse.jdt.ls.core.product",
+			"-Dlog.protocol=true",
+			"-Dlog.level=ALL",
+			"-Xms1g",
+			"--add-modules=ALL-SYSTEM",
+			"--add-opens",
+			"java.base/java.util=ALL-UNNAMED",
+			"--add-opens",
+			"java.base/java.lang=ALL-UNNAMED",
+			"-jar",
+			jdtls_path .. "/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
+			"-configuration",
+			jdtls_path .. "/config_linux",
+			"-data",
+			os.getenv("HOME") .. "/.cache/my_jdtls",
+		},
+	})
 end
