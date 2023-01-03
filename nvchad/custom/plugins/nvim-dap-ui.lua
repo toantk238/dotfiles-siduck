4a7a9991f376b2cc1835430b2750ff5b8e50539b
rock-tran <rock.tran@manadr.com>
2023-01-03 11:39:03 +0700

    Load .vscode/launch.json per project

--- nvchad/custom/plugins/nvim-dap-ui.lua
+++ nvchad/custom/plugins/nvim-dap-ui.lua
@@ -1,5 +1,10 @@
 local dap, dapui = require("dap"), require("dapui")
 dapui.setup()
+
+if vim.fn.filereadable(".vscode/launch.json") then
+	require("dap.ext.vscode").load_launchjs()
+end
+
 dap.listeners.after.event_initialized["dapui_config"] = function()
 	dapui.open()
 end
