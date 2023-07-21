95e2d5e4cd574ddf5921c9c6707fff3025984dcc
rock-tran <rock.tran@manadr.com>
2023-07-21 17:52:45 +0700

    Update

--- nvchad/custom/configs/neodev.lua
+++
@@ -1,9 +0,0 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
-
-local lspconfig = require("lspconfig")
-
-lspconfig.lua_ls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
