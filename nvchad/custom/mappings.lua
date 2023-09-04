0e3cf414ef28a7df1c830df0b462b1991e5c2ff8
rock-tran <rock.tran@manadr.com>
2023-09-04 16:05:02 +0700

    Update

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -110,13 +110,14 @@ M.lsp = {
 		},
 		["<leader>fi"] = {
 			function()
-				local params = vim.lsp.util.make_range_params()
+				local offset_encoding = vim.lsp.util._get_offset_encoding(0)
+				local params = vim.lsp.util.make_range_params(nil, offset_encoding)
 				params.context = { only = { "source.organizeImports" } }
 				local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 500)
 				for _, res in pairs(result or {}) do
 					for _, r in pairs(res.result or {}) do
 						if r.edit then
-							vim.lsp.util.apply_workspace_edit(r.edit, "utf-8")
+							vim.lsp.util.apply_workspace_edit(r.edit, offset_encoding)
 						else
 							vim.lsp.buf.execute_command(r.command)
 						end
