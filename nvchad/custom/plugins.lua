0be7cb0852049225d1c96292206b134ae9a290f0
rock-tran <rock.tran@manadr.com>
2024-01-12 00:54:28 +0700

    Update

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -107,11 +107,12 @@ return {
 					files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
 					-- -- optional entries:
 					branch = "main", -- default branch in case of git repo if different from master
-					generate_requires_npm = true, -- if stand-alone parser without npm dependencies
+					generate_requires_npm = false, -- if stand-alone parser without npm dependencies
 					requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
 				},
 				-- filetype = "strings", -- if filetype does not match the parser name
 			}
+			vim.api.nvim_set_hl(0, "@string.strings", { link = "String" })
 			-- vim.treesitter.language.register("applestrings", "strings")
 		end,
 	},
