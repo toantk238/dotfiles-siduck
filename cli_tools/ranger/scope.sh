6b425444d889d3eac88556145f1f7356d98f03ca
rock-tran <rock.tran@manadr.com>
2025-05-14 17:50:09 +0700

    Allow to preview PDF; set max_size in preview

--- cli_tools/ranger/scope.sh
+++ cli_tools/ranger/scope.sh
@@ -190,14 +190,14 @@ handle_image() {
         #       "${IMAGE_CACHE_PATH}" && exit 6;;
 
         ## PDF
-        # application/pdf)
-        #     pdftoppm -f 1 -l 1 \
-        #              -scale-to-x "${DEFAULT_SIZE%x*}" \
-        #              -scale-to-y -1 \
-        #              -singlefile \
-        #              -jpeg -tiffcompression jpeg \
-        #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-        #         && exit 6 || exit 1;;
+        application/pdf)
+            pdftoppm -f 1 -l 1 \
+                     -scale-to-x "${DEFAULT_SIZE%x*}" \
+                     -scale-to-y -1 \
+                     -singlefile \
+                     -jpeg -tiffcompression jpeg \
+                     -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
+                && exit 6 || exit 1;;
 
 
         ## ePub, MOBI, FB2 (using Calibre)
